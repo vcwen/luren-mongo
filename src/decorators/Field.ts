7ce2b3a892bc9cb09edd14257de056a2a60cf537
Vincent Wen <wenwei1202@gmail.com>
2019-03-25 22:44:44 +0800

    feat(field): read prop from PropMetadata

--- src/decorators/Field.ts
+++ src/decorators/Field.ts
@@ -1,31 +1,22 @@
-import { Map } from 'immutable'
 import 'reflect-metadata'
 import { MetadataKey } from '../constants/MetadataKey'
 
 export interface IFieldOptions {
-  type: any
+  type?: any
+  schema?: any
   required: boolean
   default: any
 }
 
 export class FieldMetadata {
-  public type: string = 'string'
-  public required: boolean = false
-  public default: any
+  public schema?: string
+  public required?: boolean
+  public default?: any
 }
 
 export function Field(options?: IFieldOptions) {
   return (target: object, propertyKey: string) => {
-    if (options) {
-      let fields: Map<string, FieldMetadata> = Reflect.getMetadata(MetadataKey.FIELD, target.constructor) || Map()
-      if (!options.type) {
-        options.type = String
-      }
-      fields = fields.set(propertyKey, options)
-      Reflect.defineMetadata(MetadataKey.FIELD, fields, target.constructor)
-    } else {
-      const fields: Map<string, FieldMetadata> = Reflect.getMetadata(MetadataKey.FIELD, target.constructor) || Map()
-      Reflect.defineMetadata(MetadataKey.FIELD, fields, target.constructor)
-    }
+    const metadata = options || {}
+    Reflect.defineMetadata(MetadataKey.FIELD, metadata, target.constructor, propertyKey)
   }
 }
