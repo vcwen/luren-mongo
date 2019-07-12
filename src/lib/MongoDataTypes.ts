d2bf5df0129b200b527bc6c35c9862c6ac5fccf0
Vincent Wen <wenwei1202@gmail.com>
2019-07-12 23:07:40 +0800

    refactor(DataTypes): upgrade deps

--- src/lib/MongoDataTypes.ts
+++ src/lib/MongoDataTypes.ts
@@ -1,22 +1,18 @@
-import { createPersistDataTypes, IPersistSchema, IPersistTypeOptions } from 'luren-schema'
+import { createPersistDataTypes, IPersistSchema, ITypeOptions } from 'luren-schema'
 import { ObjectId, ObjectID } from 'mongodb'
 import { defineMongoSchema } from './utils'
 
 export const MongoDataTypes = createPersistDataTypes()
 
 const objectIdSchema: IPersistSchema = {
-  type: 'object',
-  validate: (_1, val): [boolean, string] => {
-    if (val === undefined || ObjectId.isValid(val)) {
-      return [true, '']
-    } else {
-      return [false, `Invalid ObjectId: ${val}`]
-    }
-  }
+  type: 'objectId'
 }
 defineMongoSchema(ObjectId, objectIdSchema)
 defineMongoSchema(ObjectID, objectIdSchema)
-class ObjectIdPersistTypeOptions implements IPersistTypeOptions {
+class ObjectIdPersistTypeOptions implements ITypeOptions {
+  public toJsonSchema() {
+    return { type: 'objectId' }
+  }
   public validate(_1: IPersistSchema, val: any): [boolean, string] {
     if (val === undefined || ObjectId.isValid(val)) {
       return [true, '']
